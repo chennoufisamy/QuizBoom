0a574b34cd4697bb19b8d9744390fc7ae105c0a3
Samy Chennoufi <71331368+chennoufisamy@users.noreply.github.com>
2024-04-02 15:51:42 +0200

    Add files via upload

---
+++ QuizzBoom/QuizzBoom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizzBoom
+//
+//  Created by Samy Chennoufi on 02/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
