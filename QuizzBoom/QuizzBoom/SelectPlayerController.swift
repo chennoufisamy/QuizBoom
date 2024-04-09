231a169bda25444162d9b670163bf0d01c14a1d0
Samy Chennoufi <samychennoufi18@gmail.com>
2024-04-09 16:01:38 +0200

    Création interface quiz et attaque

---
+++ QuizzBoom/QuizzBoom/SelectPlayerController.swift
@@ -0,0 +1,34 @@
+//
+//  SelectPlayerController.swift
+//  QuizzBoom
+//
+//  Created by Samy Chennoufi on 09/04/2024.
+//
+
+import UIKit
+
+class SelectPlayerController: UIViewController {
+
+    @IBOutlet var grilleJoueur1: [UIImageView]!
+    
+
+    @IBOutlet var grilleJoueur2: [UIImageView]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
